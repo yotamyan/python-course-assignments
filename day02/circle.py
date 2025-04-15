75033e05e2f4889c2b94cc4c019ac6cd851d9ab1
Yotam Yaniv <yotamyaniv@Yotams-MacBook-Air.local>
2025-04-15 18:32:32 +0300

    Initial commit with updated circle and rectangle scripts

---
+++ day02/circle.py
@@ -0,0 +1,8 @@
+import math
+
+r = float(input("What is the radius of your circle?"))
+A = math.pi * r ** 2
+C = 2 * math.pi * r
+
+print("The area of your circle is: " + A)
+print("Then circumference of your circle is: " + C)
---
+++ day03/circle.py
@@ -0,0 +1,9 @@
+import math
+import sys
+
+r = float(sys.argv[1])
+A = math.pi * r ** 2
+C = 2 * math.pi * r
+
+print(f"The area of your circle is: {A}")
+print(f"The circumference of your circle is: {C}")
