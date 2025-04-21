e02c98c7441bdf7ecad693b4a86a95bbc687ee35
Yotam Yaniv <yotamyaniv@Yotams-Air.lan>
2025-04-21 17:06:04 +0300

    correction

--- day02/rectangle.py
+++ day02/rectangle.py
@@ -1,8 +1,8 @@
-height = float(input("Enter the height of your rectangle"))
-width = float(input("Enter the width of your rectangle"))
+height = float(input("Enter the height of your rectangle: "))
+width = float(input("Enter the width of your rectangle: "))
 
 P = 2 * height + 2 * width
 A = height * width
 
-print("The perimeter of your rectangle is: " + P)
-print("The area of your rectangle is: " + A)
+print(f"The perimeter of your rectangle is: {P}")
+print(f"The area of your rectangle is: {A}")
