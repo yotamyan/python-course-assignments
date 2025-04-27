43dfca0e4ef552f2e0dd195e27a7659b9294ec71
Yotam Yaniv <yotamyaniv@Yotams-MacBook-Air.local>
2025-04-27 16:01:27 +0300

    update

--- day03/number_guessing_game.py
+++ day03/number_guessing_game.py
@@ -1,7 +1,7 @@
 import random
 
 num = random.randint(1, 20)
-guess = input("Guess a number between 1 and 20: ")
+guess = int(input("Guess a number between 1 and 20: "))
 
 if guess == num:
     print("You guessed the correct number!")
